2513ae29f15491ce51f747c0a2c38b6b6ac6343a
Koustav Ray <koustav.ray@here.com>
2020-04-27 04:14:53 +0530

    Delivery 3.5

---
+++ assets/texts/captions.js
@@ -0,0 +1,129 @@
+const jsonCaptions={
+  "en": [
+    {
+      "title": "SDG 1. No Poverty",
+      "caption": "'Poverty is the worst form of violence!' - Mahatma Gandhi "
+    },
+    {
+      "title": "SDG 2. Zero Hunger",
+      "caption": "'If you can not feed a hundred people, then just feed one.'- Mother Teresa"
+    },{
+      "title": "SDG 3. Good Health and Well Being",
+      "caption": "'The first wealth is health'- Ralph Waldo Emerson"
+    },
+    {
+      "title": "SDG 4: Quality Education",
+      "caption": "'An investment in knowledge pays the best interest.'- Benjamin Franklin"
+    },
+    {
+      "title": "SDG 5: Gender Equality",
+      "caption": "'Gender Equality is a human fight, not a female fight.' - Freida Pinto"
+    },
+    {
+      "title": "SDG 6: Clean Water and Sanitation",
+      "caption": "'Thousands have lived without love, not one without water.'- W.H.Auden"
+    },{
+      "title": "SDG 7: Affordable and Clean Energy",
+      "caption": "'The nation that leads in renewable energy will be the nation that leads the world.'- James Cameron"
+    },
+    {
+      "title": "SDG 8: Decent Work and economic growth",
+      "caption": "'Economic growth means nothing if it leaves people out.' - Jack Kemp"
+    },{
+      "title": "SDG 9: Industry, innovation and infrastructure",
+      "caption": "'Never before in history has innovation offered the promise of so much to so many in so short a time.'- Bill Gates"
+    },
+    {
+      "title": "SDG 10: Reduced Inequalities",
+      "caption": "'Without equality, there cannot be liberty.'- Harold Laski"
+    },
+    {
+      "title": "SDG 11: Sustainable Cities and Communities",
+      "caption": "'First life, then spaces, then buildings- the other way around never works.'- Jan Gehl"
+    },
+    {
+      "title": "SDG 12: Responsible Consumption and Production",
+      "caption": "'Infinite growth of material consumption in a finite world is an impossibility'- E.F.Schumacher"
+    },
+    {
+      "title": "SDG 13: Climate Action",
+      "caption": "'No challenge poses a greater threat to future generations than climate change' - Barack Obama"
+    },{
+      "title": "SDG 14: Life Below Water",
+      "caption": "'The world's finest wilderness lies below the waves'- Wyland"
+    },
+    {
+      "title": "SDG 15: Life on Land",
+      "caption": "'Eventually we'll realize that if we destroy the ecosystem, we destroy ourselves'- Jonas Salk"
+    },{
+      "title": "SDG 16: Peace, Justice and Strong Institutions",
+      "caption": "'World peace begins with inner peace.'- Dalai Lama"
+    },{
+      "title": "SDG 17: Partnerships for the Goals",
+      "caption": "'Competition makes us faster, collaboration makes us better.'- Fyrefly"
+    }
+  ],
+  "de": [
+    {
+      "title": "Ziel 1 - Keine Armut",
+      "caption": "'Armut ist die schlimmste Form der Gewalt' - Mahatma Gandhi"
+    },
+    {
+      "title": "Ziel 2 - Kein Hunger",
+      "caption": "'Wenn du nicht hundert Menschen ernähren kannst, dann ernähre nur einen.' - Mutter Teresa"
+    },{
+      "title": "Ziel 3 - Gesundheit und Wohlergehen",
+      "caption": "'Der erste Reichtum ist die Gesundheit' - Ralph Waldo Emerson"
+    },
+    {
+      "title": "Ziel 4 - Hochwertige Bildung",
+      "caption": "'Eine Investition in Wissen zahlt die besten Zinsen' - Benjamin Franklin"
+    },{
+      "title": "Ziel 5 - Geschlechter Gleichheit",
+      "caption": "'Die Gleichstellung der Geschlechter ist ein Kampf der Menschen, kein Kampf der Frauen'. - Freida Pinto"
+    },
+    {
+      "title": "Ziel 6 - Sauberes Wasser und Sanitäreinrichtungen",
+      "caption": "'Tausende haben ohne Liebe gelebt, nicht einer ohne Wasser' - W.H.Auden"
+    },{
+      "title": "Ziel 7 - Bezahlbare und saubere Energie",
+      "caption": "'Die Nation, die bei den erneuerbaren Energien führend ist, wird die Nation sein, die die Welt führt' - James Cameron"
+    },
+    {
+      "title": "Ziel 8: Menschenwürdige Arbeit und Wirtschaftswachstum",
+      "caption": "'Wirtschaftswachstum bedeutet nichts, wenn es die Menschen außen vor lässt'. - Jack Kemp"
+    },{
+      "title": "Ziel 9 - Industrie, Innovation und Infrastruktur",
+      "caption": "'Noch nie zuvor in der Geschichte hat die Innovation so vielen Menschen in so kurzer Zeit so viel versprochen' - Bill Gates"
+    },
+    {
+      "title": "Ziel 10 - Weniger Ungleichheiten",
+      "caption": "'Ohne Gleichheit kann es keine Freiheit geben' - Harold Laski"
+    },{
+      "title": "Ziel 11 - Nachhaltige Städte und Gemeinden",
+      "caption": "'Erst Leben, dann Räume, dann Gebäude - umgekehrt funktioniert es nie' - Jan Gehl"
+    },
+    {
+      "title": "Ziel 12 - Nachhaltige/r Konsum und Produktion",
+      "caption": "'Unendliches Wachstum des Materialverbrauchs in einer endlichen Welt ist eine Unmöglichkeit' - E.F.Schumacher"
+    },
+    {
+      "title": "Ziel 13 - Maßnahmen zum Klimaschutz",
+      "caption": "'Keine Herausforderung stellt eine größere Bedrohung für künftige Generationen dar als der Klimawandel' - Barack Obama"
+    },
+    {
+      "title": "Ziel 14 - Leben unter Wasser",
+      "caption": "'Die schönste Wildnis der Welt liegt unter den Wellen' - Wyland"
+    },{
+      "title":"Ziel 15 - Leben an Land",
+      "caption": "'Irgendwann werden wir erkennen, dass wir uns selbst zerstören, wenn wir das Ökosystem zerstören' - Jonas Salk"
+    },
+    {
+      "title": "Ziel 16 - Frieden, Gerechtigkeit und starke Institutionen",
+      "caption": "'Weltfrieden beginnt mit innerem Frieden' - Dalai Lama"
+    },{
+      "title": "Ziel 17 – Partnerschaften zur Erreichung der Ziele",
+      "caption": "'Wettbewerb macht uns schneller, Zusammenarbeit macht uns besser' - Fyrefly"
+    }
+  ]
+}
